f8682d815f38afb755fd39dd842985a92dfd4992
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-23 12:30:12 -0400

    Add Create View, update Restaurant Controller

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -14,7 +14,28 @@ namespace RestaurantRater.Controllers
         // GET: Restaurant/Index
         public ActionResult Index()
         {
-            return View(_db.Restaurants.ToList());
+            return View(_db.Restaurants.ToList());  
+        }
+
+        //GET: Restaurant/Create 
+        public ActionResult Create()  
+        {
+            return View();
+        }
+
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
