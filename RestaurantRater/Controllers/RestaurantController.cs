ff16bfc31f70d38bd3ff5ec55ddfcd225edcff00
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-23 15:05:37 -0400

    Add Details View, update Restaurant Controller

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -46,7 +46,7 @@ namespace RestaurantRater.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Restaurant restaurant = _db.Restaurants.Find(id);
-            if(restaurant == null)
+            if (restaurant == null)
             {
                 return HttpNotFound();
             }
@@ -91,6 +91,20 @@ namespace RestaurantRater.Controllers
                 return RedirectToAction("Index");
             }
 
+            return View(restaurant);
+        }
+        //GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
             return View(restaurant);
         }
     }
